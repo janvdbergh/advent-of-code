e2e6f078789ee0b602df45f3bfbb705dc039b4ff
Jan Van den Bergh <jan.vdbergh@gmail.com>
2017-12-08 20:25:07 +0100

    2017 Day 3.

---
+++ src/main/java/eu/janvdb/aoc2017/day3/Puzzle.java
@@ -0,0 +1,52 @@
+package eu.janvdb.aoc2017.day3;
+
+import javaslang.collection.HashMap;
+import javaslang.collection.Map;
+
+public class Puzzle {
+
+	private static final int INPUT = 265149;
+
+	public static void main(String[] args) {
+		part1();
+		part2();
+	}
+
+	private static void part1() {
+		Position position = Position.at(INPUT);
+		System.out.println(position);
+	}
+
+	private static void part2() {
+		Map<Position, Integer> sums = HashMap.of(Position.at(1), 1);
+
+		for (int value = 1; value <= Position.NUMBER_OF_POSITIONS; value++) {
+			Position position = Position.at(value);
+
+			System.out.println(position + " -> " + getSum(sums, position));
+			if (getSum(sums, position) > INPUT) {
+				break;
+			}
+
+			sums = addToSum(sums, position, 1, 0);
+			sums = addToSum(sums, position, 1, 1);
+			sums = addToSum(sums, position, 0, 1);
+			sums = addToSum(sums, position, -1, 1);
+			sums = addToSum(sums, position, -1, 0);
+			sums = addToSum(sums, position, -1, -1);
+			sums = addToSum(sums, position, 0, -1);
+			sums = addToSum(sums, position, 1, -1);
+		}
+	}
+
+	private static int getSum(Map<Position, Integer> sums, Position position) {
+		return sums.get(position).getOrElse(0);
+	}
+
+	private static Map<Position, Integer> addToSum(Map<Position, Integer> sums, Position current, int dx, int dy) {
+		Position toUpdate = Position.at(current.getX() + dx, current.getY() + dy);
+		int newSum = getSum(sums, toUpdate) + getSum(sums, current);
+		return sums.put(toUpdate, newSum);
+	}
+
+}
---
+++ src/test/java/eu/janvdb/aoc2017/day3/PositionTest.java
@@ -0,0 +1,16 @@
+package eu.janvdb.aoc2017.day3;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PositionTest {
+
+	@Test
+	public void returnsCorrectPositions() {
+		Position position1024 = Position.at(1024);
+		System.out.println(position1024);
+
+		assertThat(position1024.getDistance()).isEqualTo(31);
+	}
+}
