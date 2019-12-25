df2b43ddbaa3c927043a11c9c9ba72778de5c842
Jan Van den Bergh <jan.vdbergh@gmail.com>
2019-12-25 11:13:02 +0100

    Create abstract superclass.

--- src/main/java/eu/janvdb/aoc2019/day24/Grid.java
+++ src/main/java/eu/janvdb/aoc2019/day24/Grid.java
@@ -1,54 +1,26 @@
 package eu.janvdb.aoc2019.day24;
 
 import java.util.BitSet;
-import java.util.Objects;
 
-public class Grid {
-
-	public static final int SIZE = 5;
-
-	private final BitSet bitSet;
+public class Grid extends AbstractGrid<Grid> {
 
 	public Grid(String[] input) {
-		BitSet result = new BitSet(SIZE * SIZE);
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				result.set(index(row, column), input[row].charAt(column) == '#');
-			}
-		}
-
-		this.bitSet = result;
+		super(input);
 	}
 
 	private Grid(BitSet bitSet) {
-		this.bitSet = bitSet;
+		super(bitSet);
 	}
 
-	public Grid calculateNext() {
-		BitSet newBits = new BitSet(SIZE * SIZE);
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				int count = (getValue(row - 1, column) ? 1 : 0) +
-						(getValue(row + 1, column) ? 1 : 0) +
-						(getValue(row, column - 1) ? 1 : 0) +
-						(getValue(row, column + 1) ? 1 : 0);
-				boolean currentValue = getValue(row, column);
-				boolean newValue = (currentValue && count == 1) || (!currentValue && (count == 1 || count == 2));
-				newBits.set(index(row, column), newValue);
-			}
-		}
-
-		return new Grid(newBits);
+	@Override
+	protected Grid createGrid(BitSet bitSet) {
+		return new Grid(bitSet);
 	}
 
-	public void print() {
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				System.out.print(getValue(row, column) ? '#' : '.');
-			}
-			System.out.println();
-		}
-		System.out.println();
+	@Override
+	protected boolean getValue(int row, int column) {
+		if (row < 0 || column < 0 || row >= SIZE || column >= SIZE) return false;
+		return bitSet.get(index(row, column));
 	}
 
 	public int calculateDiversity() {
@@ -61,27 +33,5 @@ public class Grid {
 			}
 		}
 		return result;
-	}
-
-	private boolean getValue(int row, int column) {
-		if (row < 0 || column < 0 || row >= SIZE || column >= SIZE) return false;
-		return bitSet.get(index(row, column));
-	}
-
-	private int index(int row, int column) {
-		return row * SIZE + column;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Grid grid = (Grid) o;
-		return bitSet.equals(grid.bitSet);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(bitSet);
 	}
 }
