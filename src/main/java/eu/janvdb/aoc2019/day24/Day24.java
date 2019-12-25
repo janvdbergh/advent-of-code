812a1185dafdf9a4afc2f020dfa3a62a581ba806
Jan Van den Bergh <jan.vdbergh@gmail.com>
2019-12-25 10:58:34 +0100

    Refactor into class.

--- src/main/java/eu/janvdb/aoc2019/day24/Day24.java
+++ src/main/java/eu/janvdb/aoc2019/day24/Day24.java
@@ -1,6 +1,5 @@
 package eu.janvdb.aoc2019.day24;
 
-import java.util.BitSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,87 +19,29 @@ public class Day24 {
 			"..#..",
 			"#...."
 	};
-	public static final int SIZE = INPUT.length;
 
 	public static void main(String[] args) {
 		new Day24().run();
 	}
 
 	private void run() {
-		BitSet input = readInput(INPUT);
-		print(input);
+		part1();
+	}
 
-		Set<BitSet> patterns = new HashSet<>();
-		BitSet current = input;
+	private void part1() {
+		Grid grid = new Grid(INPUT);
+		grid.print();
+
+		Set<Grid> patterns = new HashSet<>();
+		Grid current = grid;
 		while (!patterns.contains(current)) {
 			patterns.add(current);
-			current = calculateNext(current);
+			current = current.calculateNext();
 
-			print(current);
+			current.print();
 		}
 
-		int result = calculateDiversity(current);
-
-		System.out.println(result);
-	}
-
-	private BitSet readInput(String[] input) {
-		BitSet result = new BitSet(SIZE * SIZE);
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				result.set(index(row, column), input[row].charAt(column) == '#');
-			}
-		}
-
-		return result;
-	}
-
-	private BitSet calculateNext(BitSet current) {
-		BitSet result = new BitSet(SIZE * SIZE);
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				int count = (getValue(current, row - 1, column) ? 1 : 0) +
-						(getValue(current, row + 1, column) ? 1 : 0) +
-						(getValue(current, row, column - 1) ? 1 : 0) +
-						(getValue(current, row, column + 1) ? 1 : 0);
-				boolean currentValue = getValue(current, row, column);
-				boolean newValue = (currentValue && count == 1) || (!currentValue && (count == 1 || count == 2));
-				result.set(index(row, column), newValue);
-			}
-		}
-
-		return result;
-	}
-
-	private void print(BitSet current) {
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				System.out.print(getValue(current, row, column) ? '#' : '.');
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-
-	private int calculateDiversity(BitSet current) {
-		int power = 1;
-		int result = 0;
-		for (int row = 0; row < SIZE; row++) {
-			for (int column = 0; column < SIZE; column++) {
-				if (getValue(current, row, column)) result += power;
-				power += power;
-			}
-		}
-		return result;
-	}
-
-	private boolean getValue(BitSet bits, int row, int column) {
-		if (row < 0 || column < 0 || row >= SIZE || column >= SIZE) return false;
-		return bits.get(index(row, column));
-	}
-
-	private int index(int row, int column) {
-		return row * SIZE + column;
+		System.out.println(current.calculateDiversity());
 	}
 
 }
