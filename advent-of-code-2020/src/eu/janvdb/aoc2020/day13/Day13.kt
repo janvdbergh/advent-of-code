ff3988574131c635c7b99d7bccbff7282da021e9
Jan Van den Bergh <jan.vdbergh@gmail.com>
2020-12-13 17:08:04 +0100

    Proper solution for day 13: solve Chinese Remainder Theorem equation

--- advent-of-code-2020/src/eu/janvdb/aoc2020/day13/Day13.kt
+++ advent-of-code-2020/src/eu/janvdb/aoc2020/day13/Day13.kt
@@ -1,7 +1,16 @@
 package eu.janvdb.aoc2020.day13
 
+import eu.janvdb.aoc2020.common.ChineseRemainderTheoremEquation
+import eu.janvdb.aoc2020.common.minusModN
+import eu.janvdb.aoc2020.common.solveChineseRemainderTheorem
+import java.math.BigInteger
+
+//const val TIME_ = 939L
+//const val BUSSES = "7,13,x,x,59,x,31,19"
+
 const val TIME = 1000052L
-const val BUSSES = "23,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,863,x,x,x,x,x,x,x,x,x,x,x,19,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,571,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41"
+const val BUSSES =
+	"23,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,863,x,x,x,x,x,x,x,x,x,x,x,19,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,571,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41"
 
 fun main() {
 	part1()
@@ -13,27 +22,26 @@ private fun part1() {
 	val earliestStartTimesPerBus = busses.map { Pair(it, (TIME + it - 1) / it * it) }
 	val earliestBus = earliestStartTimesPerBus.minByOrNull { it.second }!!
 
-	println(earliestBus)
 	println(earliestBus.first * (earliestBus.second - TIME))
 }
 
 private fun part2() {
-	val startDeltas = BUSSES.split(",")
+	val startTimes = BUSSES.split(",")
 		.mapIndexed { index, value -> Pair(index, value) }
 		.filter { it.second != "x" }
-		.map { StartDelta(it.second.toLong(), it.first.toLong()) }
+		.map { StartTime(it.second.toLong().toBigInteger(), it.first.toLong().toBigInteger()) }
 
-	println(startDeltas)
 
 	// Equations: (t + 0) mod 7 = 0, (t + 1) mod 13 = 0, (t + 4) mod 59 = 0, (t + 6) mod 31 = 0, (t + 7) mod 19 = 0
 	// Or: t = 0 (mod 7), t = 12 (mod 13), t = 54 (mod 59), t = 25 (mod 31), t = 12 (mod 19)
 	// Since the values are pairwise coprime, this can be solved with the Chinese Remainder Theorem.
-
-	// Equation solved for now with Wolfram Alpha.
+	val equations = startTimes.map(StartTime::asChineseTheoremEquation)
+	val solution = solveChineseRemainderTheorem(equations)
+	println(solution)
 }
 
-data class StartDelta(val bus: Long, val startTime: Long) {
-	override fun toString(): String {
-		return "(t + $startTime) mod $bus = 0"
+data class StartTime(val busNumber: BigInteger, val startTime: BigInteger) {
+	fun asChineseTheoremEquation() : ChineseRemainderTheoremEquation {
+		return ChineseRemainderTheoremEquation(minusModN(busNumber, startTime, busNumber), busNumber)
 	}
 }
