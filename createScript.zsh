9581d45434c1a838ef9ae3e0df25c88c3d173208
Jan Van den Bergh <jan.vdbergh@gmail.com>
2021-12-18 14:29:59 +0100

    Improve file creation script.

--- createScript.zsh
+++ createScript.zsh
@@ -1,15 +1,24 @@
 #!/bin/zsh
 
+cd ${0:a:h}
+
 YEAR=$(date +%Y)
 DAY=$(date +%d)
 
-mkdir -p advent-of-code-$YEAR/inputs
-touch advent-of-code-$YEAR/inputs/input$DAY.txt
-touch advent-of-code-$YEAR/inputs/input$DAY-test.txt
+INPUT_DIR=advent-of-code-$YEAR/inputs
+INPUT_TEST=$INPUT_DIR/input$DAY-test.txt
+INPUT=$INPUT_DIR/input$DAY.txt
+CODE_DIR=advent-of-code-$YEAR/src/main/kotlin/eu/janvdb/aoc$YEAR/day$DAY
+CODE=$CODE_DIR/Day$DAY.kt
 
-mkdir -p advent-of-code-$YEAR/src/main/kotlin/eu/janvdb/aoc$YEAR/day$DAY
-touch advent-of-code-$YEAR/src/main/kotlin/eu/janvdb/aoc$YEAR/day$DAY/Day$DAY.kt
+mkdir -p $INPUT_DIR
+touch $INPUT_TEST
+touch $INPUT
 
-git add advent-of-code-$YEAR/inputs/input$DAY.txt
-git add advent-of-code-$YEAR/inputs/input$DAY-test.txt
-git add advent-of-code-$YEAR/src/main/kotlin/eu/janvdb/aoc$YEAR/day$DAY/Day$DAY.kt
+mkdir -p $CODE_DIR
+if [ ! -f $CODE ]
+then
+  echo "package eu.janvdb.aoc$YEAR.day$DAY" > $CODE
+fi
+
+git add $INPUT_TEST $INPUT $CODE
