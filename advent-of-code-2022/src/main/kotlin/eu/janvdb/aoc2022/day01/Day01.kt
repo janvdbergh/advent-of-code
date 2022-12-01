d751347f079c4b4d4e9eedc0fe0c39707182df3d
Jan Van den Bergh <jan.vdbergh@gmail.com>
2022-12-01 06:24:45 +0100

    AOC2022.01

---
+++ advent-of-code-2022/src/main/kotlin/eu/janvdb/aoc2022/day01/Day01.kt
@@ -0,0 +1,29 @@
+package eu.janvdb.aoc2022.day01
+
+import eu.janvdb.aocutil.kotlin.readGroupedLines
+
+//val FILENAME = "input01-test.txt"
+val FILENAME = "input01.txt"
+
+fun main() {
+	part1()
+	part2()
+}
+
+private fun part1() {
+	val result = readGroupedLines(2022, FILENAME)
+		.map { lines -> lines.map { it.toInt() }.sum() }
+		.max()
+
+	println(result)
+}
+
+private fun part2() {
+	val result = readGroupedLines(2022, FILENAME)
+		.map { lines -> lines.map { it.toInt() }.sum() }
+		.sortedDescending()
+		.take(3)
+		.sum()
+
+	println(result)
+}
