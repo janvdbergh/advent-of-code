bc9e28833c1e88aeada00176286f4f3b0c1b6339
Jan Van den Bergh <jan.vdbergh@gmail.com>
2023-12-19 20:11:10 +0100

    AOC2023.19 part 2

--- advent-of-code-2023/src/main/kotlin/eu/janvdb/aoc2023/day19/day19.kt
+++ advent-of-code-2023/src/main/kotlin/eu/janvdb/aoc2023/day19/day19.kt
@@ -5,24 +5,74 @@ import eu.janvdb.aocutil.kotlin.readGroupedLines
 //const val FILENAME = "input19-test.txt"
 const val FILENAME = "input19.txt"
 
+val VARIABLES = listOf("x", "m", "a", "s")
+
+const val MIN_VALUE = 1
+const val MAX_VALUE = 4000
+
 fun main() {
     val lines = readGroupedLines(2023, FILENAME)
     val rules = Rules.parse(lines[0])
     val parts = lines[1].map(Part::parse)
 
+    part1(parts, rules)
+
+    part2(rules)
+}
+
+private fun part1(parts: List<Part>, rules: Rules) {
     val acceptedParts = parts.filter(rules::evaluate)
     println(acceptedParts.sumOf { it.rating })
 }
 
+private fun part2(rules: Rules) {
+    val ranges = VARIABLES.associateWith { MIN_VALUE..MAX_VALUE }
+    val result = rules.countAccepted("in", ranges)
+    println(result)
+}
+
 data class Rules(val rules: Map<String, Rule>) {
     fun evaluate(part: Part): Boolean {
-        var current = Outcome("in")
+        var current = "in"
         while (!current.isAccepted() && !current.isRejected()) {
-            val rule = rules[current.text]!!
+            val rule = rules[current]!!
             current = rule.evaluate(part)
         }
 
         return current.isAccepted()
+    }
+
+    fun countAccepted(outcome: String, ranges: Map<String, IntRange>, index: Int = 0): Long {
+        val spaces = (0..<index).joinToString("", "", "+-") { "| " }
+        println("$spaces$ranges -> $outcome")
+
+        if (outcome.isAccepted())
+            return 1L * ranges.values.fold(1L) { acc, range -> acc * (range.last - range.first + 1) }
+        if (outcome.isRejected())
+            return 0L
+        if (ranges.values.any { it.last < it.first })
+            return 0L
+
+        val rule = rules[outcome]!!
+
+        var currentRanges = ranges
+        var sum = 0L
+        rule.conditions.forEach { condition ->
+            val currentRange = currentRanges[condition.variable]!!
+            val newRange1 =
+                if (condition.operation == Operation.LT) currentRange.first..<condition.value else condition.value + 1..currentRange.last
+            val newRange2 =
+                if (condition.operation == Operation.LT) condition.value..currentRange.last else currentRange.first..condition.value
+
+            val newRanges = currentRanges + Pair(condition.variable, newRange1)
+            sum += countAccepted(condition.outcome, newRanges, index + 1)
+
+            currentRanges = currentRanges + Pair(condition.variable, newRange2)
+        }
+
+        sum += countAccepted(rule.defaultOutcome, currentRanges, index + 1)
+
+        return sum
     }
 
     companion object {
@@ -34,8 +84,8 @@ data class Rules(val rules: Map<String, Rule>) {
 }
 
 
-data class Rule(val name: String, val conditions: List<Condition>, val defaultOutcome: Outcome) {
-    fun evaluate(part: Part): Outcome {
+data class Rule(val name: String, val conditions: List<Condition>, val defaultOutcome: String) {
+    fun evaluate(part: Part): String {
         return conditions.find { it.matches(part) }?.outcome ?: defaultOutcome
     }
 
@@ -45,13 +95,13 @@ data class Rule(val name: String, val conditions: List<Condition>, val defaultOu
             val name = parts[0]
             val parts2 = parts[1].substring(0, parts[1].length - 1).split(",")
             val conditions = parts2.subList(0, parts2.size - 1).map(Condition::parse)
-            val defaultOutcome = Outcome(parts2.last())
+            val defaultOutcome = parts2.last()
             return Rule(name, conditions, defaultOutcome)
         }
     }
 }
 
-data class Condition(val variable: String, val operation: Operation, val value: Int, val outcome: Outcome) {
+data class Condition(val variable: String, val operation: Operation, val value: Int, val outcome: String) {
     fun matches(part: Part): Boolean {
         val partValue = part.values[variable]!!
         return operation.evaluate(partValue, value)
@@ -65,7 +115,7 @@ data class Condition(val variable: String, val operation: Operation, val value: 
                 matchResult.groupValues[1],
                 Operation.parse(matchResult.groupValues[2]),
                 matchResult.groupValues[3].toInt(),
-                Outcome(matchResult.groupValues[4])
+                matchResult.groupValues[4]
             )
         }
     }
@@ -84,13 +134,11 @@ enum class Operation(val text: String) {
     }
 }
 
-data class Outcome(val text: String) {
-    fun isAccepted() = text == "A"
-    fun isRejected() = text == "R"
-}
+fun String.isAccepted() = equals("A")
+fun String.isRejected() = equals("R")
 
 data class Part(val values: Map<String, Int>) {
-    val rating = sequenceOf("x", "m", "a", "s").sumOf { values.getOrDefault(it, 0) }
+    val rating = VARIABLES.sumOf { values.getOrDefault(it, 0) }
 
     companion object {
         private val REGEX = Regex("(\\w+)=(\\d+)")
